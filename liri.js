d001107720e3e19139e61836e6a4d15956b27d48
katieburrows <katie_burrows@me.com>
2018-02-14 14:24:38 -0700

    setup files, with proper keys; next step is logic

---
+++ liri.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+
+
+
+
+// 1-movie
+// 2/3-twitter or spotify
+// 4-do-what it says
+
+// var Twitter = require('twitter');
+
+// var Spotify = require('node-spotify-api');
+ 
+// var spotify = new Spotify({
+//   id: <your spotify client id>,
+//   secret: <your spotify client secret>
+// });
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
