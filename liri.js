ba9080f6f8fba256158160f892d65612e4837956
katieburrows <katie_burrows@me.com>
2019-05-29 17:29:51 -0600

    added dependencies and twitter

--- liri.js
+++ liri.js
@@ -1,2 +1,13 @@
 require("dotenv").config();
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api")
+var keys = require("./keys");
 
+
+
+
+
+
+var twitterImport = require("./keys.js");
+
+var spotifyImport = require("./keys.js");
