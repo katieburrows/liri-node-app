573842b7666fbaa69a601eb3e67458e923edb907
katieburrows <katie_burrows@me.com>
2019-05-30 15:22:21 -0600

    added conditional routes

--- liri.js
+++ liri.js
@@ -4,5 +4,39 @@ var Spotify = require("node-spotify-api")
 var keys = require("./keys");
 var request = require("request");
 var fs = require("fs");
-var spotify = new Spotify(keys.spotify);
-var twitter = new Twitter(keys.twitter);
+// var spotify = new Spotify(keys.spotify);
+// var twitter = new Twitter(keys.twitter);
+
+var commandArgs = process.argv[2];
+
+var myTweets = function() {
+    console.log(`tweet`);
+}
+
+var spotifyThisSong = function() {
+    console.log(`song`);
+}
+
+var movieThis = function() {
+    console.log(`movie`);
+}
+
+var doWhatItSays = function() {
+    console.log(`do what it says`);
+}
+
+if (commandArgs === "my-tweets"){
+    myTweets();
+} 
+else if (commandArgs === "spotify-this-song") {
+    spotifyThisSong();
+} 
+else if (commandArgs === "movie-this"){
+    movieThis();
+} 
+else if (commandArgs === "do-what-it-says"){
+    doWhatItSays();
+}
+else {
+    console.log(`That is not a valid entry. Please enter: "my-tweets", "spotify-this-song", "movie-this", or "do-what-it-says"`);
+}
