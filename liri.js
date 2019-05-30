ee30d5697a4de12b3fad743be031f61720f375eb
katieburrows <katie_burrows@me.com>
2019-05-30 17:07:49 -0600

    got movie search set up

--- liri.js
+++ liri.js
@@ -9,21 +9,55 @@ var twitter = new Twitter(keys.twitter);
 
 var commandArgs = process.argv[2];
 
+// * This will show your last 20 tweets and when they were created at in your terminal/bash window.
 var myTweets = function() {
     console.log(`tweet`);
 }
 
+// * This will show the following information about the song in your terminal/bash window
+     
+// * Artist(s)
+
+// * The song's name
+
+// * A preview link of the song from Spotify
+
+// * The album that the song is from
+
+// * If no song is provided then your program will default to "The Sign" by Ace of Base.
 var spotifyThisSong = function() {
     console.log(`song`);
 }
 
 var movieThis = function() {
-    console.log(`movie`);
+    var movieTitle = process.argv.slice(3).join("+");
+
+    if (!movieTitle){
+        movieTitle = "Mr. Nobody";
+    }
+
+    var queryUrl = "https://www.omdbapi.com/?t=" + movieTitle + "&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        function(response) {
+            var baseline = response.data;
+            console.log("\n********** Movie Data **********\n");
+            console.log(`     Movie title: ${baseline.Title}\n     Year released: ${baseline.Year}\n     IMDB rating: ${baseline.Ratings[0].Value}\n     Rotten Tomatoes rating: ${baseline.Ratings[1].Value}\n     Country where movie was produced: ${baseline.Country}\n     Language: ${baseline.Language}\n     Plot: ${baseline.Plot}\n     Actors: ${baseline.Actors}`); 
+            console.log("\n********************************\n");
+    })
+
 }
 
+//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+     
+// * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+     
+// * Feel free to change the text in that document to test out the feature for other commands.
 var doWhatItSays = function() {
     console.log(`do what it says`);
 }
+
+
 
 if (commandArgs === "my-tweets"){
     myTweets();
